89fbeba0229e06b49ba5266092164108f144f674
Eric Prud'hommeaux <eric+github@w3.org>
2021-01-04 11:46:15 +0100

    + skeleton

---
+++ packages/api/src/api.ts
@@ -0,0 +1,5 @@
+import { meaningOfLife } from "@hdtjs/core";
+
+// eslint-disable-next-line no-console
+console.log(meaningOfLife);
+export const meaningOfLife2 = meaningOfLife * 2;
---
+++ packages/cli/src/HDTInfo.ts
@@ -0,0 +1,5 @@
+import { meaningOfLife2 } from "@hdtjs/api";
+
+// eslint-disable-next-line no-console
+console.log(meaningOfLife2);
+
