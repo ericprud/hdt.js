06922bfa99ebab61eeb683e6f100ba742419407d
Eric Prud'hommeaux <eric+github@w3.org>
2021-01-06 02:22:12 +0100

    + commander

--- packages/cli/src/HDTInfo.ts
+++ packages/cli/src/HDTInfo.ts
@@ -1,7 +1,31 @@
 import { CoreApi } from '@hdtjs/api';
+import { Command } from 'commander';
+
+class HDTInfo {
+    parameters: String[] = [];
+    static showVersion: boolean;
+    hdtInput: String;
+    execute(): void {
+    }
+}
 
 function main(argv: string[]) {
-    console.log(CoreApi() + ' HDTInfo', argv.slice(2));
+    const hdtInfo: HDTInfo = new HDTInfo();
+    com: Command/* = new Command()*/;
+    if (HDTInfo.showVersion) {
+        console.log("0.0");
+        process.exit(0);
+    }
+
+    try {
+        if (hdtInfo.hdtInput === undefined)
+            hdtInfo.hdtInput = hdtInfo.parameters[0];
+    } catch (e) {
+        process.exit(1);
+    }
+
+    hdtInfo.execute();
+    console.log(CoreApi() + ' HDTInfo', argv.slice(2), hdtInfo);
 }
 
 if (process.mainModule === module) {
